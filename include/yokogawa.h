e790cb9bcef293ad64b92a1db585c4fa49d5403b
dflay <dflay27@gmail.com>
2017-04-12 15:37:01 -0500

    Adding data members to the yokogawa data structure

--- include/yokogawa.h
+++ include/yokogawa.h
@@ -2,14 +2,17 @@
 #define YOKOGAWA_H
 
 // a data structure for the Yokogawa data 
+// to be used when generating a ROOT file for a run  
 
 typedef struct yokogawa{
    long int timestamp;    // UTC timestamp 
-   double current;        // in mA  
+   int is_manual;         // manual mode (1) or auto [fixed probe input] (0) 
+   int output_enabled;    // is output enabled (1) or disabled (0)  
+   double current;        // current read back from device in mA  
    double p_fdbk;         // coefficient for feedback    
    double i_fdbk;         // coefficient for feedback
    double d_fdbk;         // coefficient for feedback
-   double setpoint;       // user-defined setpoint  
+   double setpoint;       // user-defined setpoint in mA  
 } yokogawa_t; 
 
 #endif 
