d21722d8ff6def16be03d25626ec30e980f9082d
dflay <dflay27@gmail.com>
2017-04-13 08:06:54 -0500

    Updated the yokogawa_event data structure

--- include/yokogawa_event.h
+++ include/yokogawa_event.h
@@ -1,10 +1,10 @@
-#ifndef YOKOGAWA_H
-#define YOKOGAWA_H
+#ifndef YOKOGAWAEVENT_H
+#define YOKOGAWAEVENT_H
 
-// a data structure for the Yokogawa data 
+// a data structure for a Yokogawa event  
 // to be used when generating a ROOT file for a run  
 
-typedef struct yokogawa{
+typedef struct yokogawa_event{
    long int timestamp;    // UTC timestamp 
    int is_manual;         // manual mode (1) or auto [fixed probe input] (0) 
    int output_enabled;    // is output enabled (1) or disabled (0)  
@@ -13,6 +13,6 @@ typedef struct yokogawa{
    double i_fdbk;         // coefficient for feedback
    double d_fdbk;         // coefficient for feedback
    double setpoint;       // user-defined setpoint in mA  
-} yokogawa_t; 
+} yokogawa_event_t; 
 
 #endif 
