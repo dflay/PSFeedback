2831df14ffe9735d69886d2e79b01ae741ca098b
dflay <dflay27@gmail.com>
2017-04-13 08:05:32 -0500

    Removed an old file

--- compile.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/python 
-
-import os
-
-executable = "yoko_simple"; 
-cmd        = "gcc -Wall -I/usr/include/python2.6 -lpython2.6 {0}.c -o {0}".format(executable); 
-print cmd   
-os.system(cmd) 
