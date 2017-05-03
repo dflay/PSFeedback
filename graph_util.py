353ce161a2ee77a3eb314c071ef0fe5da1f14ba9
dflay <dflay27@gmail.com>
2017-05-03 09:01:48 -0500

    Small updates

--- graph_util.py
+++ graph_util.py
@@ -1,27 +1,14 @@
 #! /usr/bin/python 
-# miscellaneous functions for the Yokogawa operation 
+# functions used when plotting yokogawa data  
 
-import os.path 
-import sys
-import numpy as np
 import datetime
-import random
 import pytz
 import pyqtgraph as pg
 
-UNIX_EPOCH_naive        = datetime.datetime(1970, 1, 1, 0, 0) # offset-naive datetime
+UNIX_EPOCH_naive        = datetime.datetime(1970, 1, 1, 0, 0)                    # offset-naive datetime
 UNIX_EPOCH_offset_aware = datetime.datetime(1970, 1, 1, 0, 0, tzinfo = pytz.utc) # offset-aware datetime
 UNIX_EPOCH              = UNIX_EPOCH_naive
 TS_MULT_us              = 1e6
-
-#_______________________________________________________________________________
-# Miscellaneous functions and classes 
-
-def get_random(minVal,maxVal): 
-    return random.randrange(minVal,maxVal)
-
-def now_timestamp(ts_mult=TS_MULT_us, epoch=UNIX_EPOCH):
-    return(int((datetime.datetime.utcnow() - epoch).total_seconds()*ts_mult))
 
 def int2dt(ts, ts_mult=TS_MULT_us):
     return(datetime.datetime.utcfromtimestamp(float(ts)/ts_mult))
